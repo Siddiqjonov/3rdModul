b1a5858e225949271c5710c0d604f37ab4801a68
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-01-31 10:47:25 +0500

    3.7-hm Thread and Regex

---
+++ DisplayAndWriteFiles/DisplayAndWriteFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DisplayAndWriteFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Users\ACer\Downloads\Telegram Desktop\Files";
+            var files = Directory.GetFiles(directoryPath);
+
+            foreach (var file in files)
+            {
+                if (file.Contains("result")) continue;
+                new Thread(() => DisplayAndWriteFiles(file)).Start();
+            }
+        }
+
+        public static Object _lock = new Object();
+
+        public static void DisplayAndWriteFiles(string path)
+        {
+            var savePath = @"C:\Users\ACer\Downloads\Telegram Desktop\Files\result.txt";
+            lock (_lock)
+            {
+                var count = File.ReadAllText(path).Count(ch => char.IsDigit(ch));
+                var line = $"File: {path.Substring(path.LastIndexOf("\\") + 1)}, Digits: {count}, Thread ID: {Thread.CurrentThread.ManagedThreadId}";
+                Console.WriteLine(line);
+                using StreamWriter stream = new StreamWriter(savePath, true);
+                stream.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ DisplayAndWriteFiles/Regex/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidateName("Abror"));
+            Console.WriteLine(ValidateName("abror"));
+            Console.WriteLine(ValidateName("ab ror"));
+            Console.WriteLine(ValidateName("ab1ror"));
+            Console.WriteLine(ValidateName("aBror"));
+            Console.WriteLine(ValidateName("AbrorfkjlkflkKfklsadfsfsdf"));
+        }
+        public static bool ValidateName(string name)
+        {
+            var namePattern = @"^[A-Z]{1}[a-z]+$";
+            var res = Regex.IsMatch(name, namePattern);
+            // Match an Email Address
+            string emailPattern = @"^[\w\.-]+@[\w\.-]+\.\w+$";
+
+            // Match a Phone Number (Format: 123-456-7890)
+            string phonePattern = @"^\d{3}-\d{3}-\d{4}$";
+
+            // Match a URL
+            string urlPattern = @"^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$";
+
+            // Match a Number (Integer or Decimal)
+            string numberPattern = @"^-?\d+(\.\d+)?$";
+
+            // Match a Password (At Least 8 Characters, 1 Letter, 1 Number)
+            string passwordPattern = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+            return res;
+
+
+        }
+    }
+}
